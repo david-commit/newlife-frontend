99c7673844321920c83e75133bb4dec9445a385a
david-commit <david.ondiege@students.moringaschool.com>
2022-12-25 16:35:57 +0300

    Structure & Style[Patient dashbpard] Created and styled inclusing sidebar routing

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -9,9 +9,52 @@ function Patient() {
       <div className='patient-details-dash-section'>
         <h1>Hi name,</h1>
         <p>See your personal details below</p>
-        <div className="patient-details-section">
-          <h1>Bio:</h1>
-          
+        <br />
+        <div className='patient-details-section'>
+          <p className='patient-details-title'>
+            <span>Bio:&nbsp;</span>
+          </p>
+
+          <textarea readOnly id='bio'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            alias, voluptatum vero eaque veniam inventore nihil possimus
+            repellendus nemo commodi cupiditate eum praesentium incidunt odit
+            tenetur consequatur corporis cumque id?
+          </textarea>
+          <br />
+
+          <p className='patient-details-title'>
+            <span>Age:&nbsp;</span>
+            34
+          </p>
+          <p className='patient-details-title'>
+            <span>Height:&nbsp;</span>2 meters
+          </p>
+          <p className='patient-details-title'>
+            <span>Weight:&nbsp;</span>
+            68kg
+          </p>
+          <p className='patient-details-title'>
+            <span>BMI:&nbsp;</span>
+            18
+          </p>
+          <p className='patient-details-title'>
+            <span>Blood Group:&nbsp;</span>
+            O-
+          </p>
+        </div>
+      </div>
+      <div className='patient-details-notification-section'>
+        <h2>Notifications</h2>
+        <div className='patient-details-notification'>
+          <h4>Upcomming appointment (in 2hrs)</h4>
+          <p>Dr. Grace Laura (Psychologist)</p>
+          <hr />
+        </div>
+        <div className='patient-details-notification'>
+          <h4>Upcomming appointment (in 3hrs)</h4>
+          <p>Dr. Faith Ondiege (Nutritionist)</p>
+          <hr />
         </div>
       </div>
     </div>
