cd260bd92d5a253b9d6cdf6091ed32b75b53ff4f
david-commit <david.ondiege@students.moringaschool.com>
2022-12-26 17:48:36 +0300

    Feature[GET] Successful request to diaplay all products

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 
 function Shop() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+  console.log(products);
   return (
     <div className='shop-main-container'>
-      Shop Title
+      <h1>Shop</h1>
+      <div className='shop-search-filter-container'>
+        <input
+          type='search'
+          id='search-input'
+          placeholder='Search'
+          style={{
+            fontSize: '16px',
+            padding: '4px',
+            borderRadius: '5px',
+            border: '1px solid grey',
+          }}
+        />
+        <select id='shop-filter'>
+          <option hidden>Filter</option>
+          <option>Price: Low to High</option>
+          <option>Price: High to Low</option>
+        </select>
+      </div>
       <div className='shop-cards'>
-        <div className='shop-card'></div>
-        <div className='shop-card'></div>
-        <div className='shop-card'></div>
-        <div className='shop-card'></div>
-        <div className='shop-card'></div>
-        <div className='shop-card'></div>
+        {products.map((product) => {
+          return <div className='shop-card'>
+            <img src={product.image} alt="Product" />
+          </div>;
+        })}
       </div>
     </div>
   );
