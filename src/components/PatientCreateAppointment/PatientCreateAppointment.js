58cf2c7e01668d24d050df1000f74a519e962713
David <david.njoroge@student.moringaschool.com>
2023-01-10 09:25:19 +0300

    create appointment [form]

--- src/components/PatientCreateAppointment/PatientCreateAppointment.js
+++ src/components/PatientCreateAppointment/PatientCreateAppointment.js
@@ -8,7 +8,31 @@ function PatientCreateAppointment() {
       <PatientSidebar />
       <div className='patient-create-appointment-container'>
         <h1>Book an appointment</h1>
+
+        <form className='appointment-form'>
+          <select>
+            <option hidden>Select Doctor</option>
+            <option>Dr. Grace Laura</option>
+            <option>Dr. Grace Laura</option>
+            <option>Dr. Grace Laura</option>
+          </select>
+          <select>
+            <option hidden>Select type of appointment</option>
+            <option>Consultation</option>
+            <option>Dermatology</option>
+            <option>Nutrition</option>
+            <option>Pediatrics</option>
+          </select>
+          <input type='date' />
+          <textarea placeholder='Provide information on the apppointment'></textarea>
+          <br />
+          <div className='form-buttons'>
+            <button type='submit'>Submit Request</button>
+            <button type='reset'>Reset</button>
+          </div>
+        </form>
       </div>
+    
     </div>
   );
 }
