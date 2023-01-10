353d81abd45605384a3ac6ad6d090e54161c0537
David <david.njoroge@student.moringaschool.com>
2023-01-10 09:29:39 +0300

    added patient sidebar route

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Patient.css';
+import PatientSidebar from '../PatientSidebar/PatientSidebar';
+
 
 function Patient() {
   return (
     <div className='patient-main-container'>
-      
+      <PatientSidebar />
       <div className='patient-details-dash-section'>
         <h1>Hi John,</h1>
         
