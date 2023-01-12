2d17b8b88e0eff9aae11066cb4507cbe8f3a899a
David <david.njoroge@student.moringaschool.com>
2023-01-12 08:11:49 +0300

    Structure[Patient review]

--- src/components/PatientReviews/PatientReviews.js
+++ src/components/PatientReviews/PatientReviews.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import './PatientReviews.css'
 import PatientSidebar from '../PatientSidebar/PatientSidebar';
+import ReactStars from 'react-stars';
 
 function PatientReviews() {
   return (
-    <div className='patient-main-container'>
+    <div className='patient-reviews-main-container'>
       <PatientSidebar />
-      <div className='patient-existing-appointments'>
-        <h1>Your Practitioner Reviews</h1>
-      </div>
-    </div>
+      <div className='patient-review-section'>
+        
+          <textarea >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            alias, voluptatum vero eaque veniam inventore nihil possimus
+            repellendus nemo commodi cupiditate eum praesentium incidunt odit
+            tenetur consequatur corporis cumque id?
+          </textarea>
+
+            <div >
+            <button className='form-button' type='view'>View</button>
+            </div>
+            <h3>Rating</h3>
+          {/* <p>{product.rating.rate}</p> */}
+          {/* <p>{product.rating}</p> */}
+          <section className='prevRating'>
+            <p id='product-rating'>
+              <strong>{4}{"/5"}</strong>
+            </p>
+            <div>
+              <ReactStars
+                count={5}
+                value={4}
+                size={24}
+                color2={'#ffd700'}
+                half={false}
+                edit={false}
+              />
+           
+            </div>
+          </section>
+            
+        </div>
+        </div>
+        
   );
 }
 
