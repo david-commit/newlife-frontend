2543bc381b571b1f4cd1eaac46ece73449dbab3d
Vincent Makokha <makokhavomondi@gmail.com>
2023-01-13 15:30:40 +0300

    Implement appointment creation by patient

--- src/components/PatientAppointments/PatientAppointments.js
+++ src/components/PatientAppointments/PatientAppointments.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PatientSidebar from '../PatientSidebar/PatientSidebar';
 import "./PatientAppointments.css"
 import { useHistory } from 'react-router-dom';
@@ -16,41 +16,32 @@ function PatientAppointments({loggedIn, userType}) {
     history.push('/login')
   }
 
+  useEffect(()=> {
+
+  }, [])
+
   return (
     <div className='patient-appointments-main-container'>
       <PatientSidebar loggedIn={loggedIn} userType={userType} />
       <div className="patient-existing-appointments">
         <h1>Existing Appointments</h1>
 
-        <div className='patients-appointments'>
-          <br></br>
-          <p><h3>Dr. Grace Laura - General Consultation</h3>
-          <h4>2:00pm</h4></p>
-          <button type='View'>View </button>
-            <button type='Delete'>Delete</button>
-            
-          
-        <div className='patients-appointments'>
-          
-          <p><h3>Dr. Grace Laura - General Consultation</h3>
-          <h4>2:00pm</h4></p>
-          <button type='View'>View </button>
-            <button type='Delete'>Delete</button>
-         
-        </div>
-        
-        <div className='patients-appointments'>
-        
-          <p><h3>Dr. Grace Laura - General Consultation</h3>
-          <h4>2:00pm</h4></p>
-          <button type='View'>View </button>
-            <button type='Delete'>Delete</button>
+        {/* {
+          practitioners.map(practitioner => {
+            const department = practitioner.department?.name
+            const lastName = practitioner.practitioner_profiles[0]?.last_name
 
-         
-          
-        </div>
-       
-        </div>
+            return (
+              <div className='patients-appointments'>
+                <br></br>
+                <p><h3>{`Dr. ${lastName} (${department} Department)`}</h3>
+                  <h4>2:00pm</h4></p>
+                <button type='View'>View </button>
+                <button type='Delete'>Delete</button>
+              </div>
+            )
+          }).slice(0, 5)
+        } */}
       </div>
     </div>
   );
