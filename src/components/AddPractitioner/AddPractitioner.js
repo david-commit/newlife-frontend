7c70ee0e04d5f056a55969ac0fe4da40469275b1
david-commit <david.ondiege@students.moringaschool.com>
2023-01-18 01:09:23 +0300

    FAILSAFE[Add Practitioner] Before Tabs implementation incase of a failed outcome

--- src/components/AddPractitioner/AddPractitioner.js
+++ src/components/AddPractitioner/AddPractitioner.js
@@ -98,10 +98,56 @@ const AddPractitioner = () => {
 
   bmi = (weight / height / height).toFixed(1);
 
+  // const header = () => {
+  //   return(
+  //     <>
+  //     <li>ikygukd</li>
+  //     <li>ikygukd</li>
+  //     <li>ikygukd</li>
+  //     <li>ikygukd</li>
+  //     </>
+  //   )
+  // }
+  // const data = [
+  //   { id: '1', tabTitle: 'Tab 1', tabContent: header() },
+  //   { id: '2', tabTitle: 'Tab 2', tabContent: 'Tab Content 2' },
+  // ];
+
+
+  // function Tab(props) {
+  //   const [visibleTab, setVisibleTab] = React.useState(props.data[0].id);
+
+  //   const listTitles = props.data.map((item) => (
+  //     <li
+  //       onClick={() => setVisibleTab(item.id)}
+  //       className={
+  //         visibleTab === item.id ? 'tab-title tab-title--active' : 'tab-title'
+  //       }
+  //     >
+  //       {item.tabTitle}
+  //     </li>
+  //   ));
+
+  //   const listContent = props.data.map((item) => (
+  //     <p style={visibleTab === item.id ? {} : { display: 'none' }}>
+  //       {item.tabContent}
+  //     </p>
+  //   ));
+
+  //   return (
+  //     <div className='tabs'>
+  //       <ul className='tabs-titles'>{listTitles}</ul>
+  //       <div className='tab-content'>{listContent}</div>
+  //     </div>
+  //   );
+  // }
+
+
   return (
     <div className='all-practitioners-main-container'>
       <AdminSidebar />
       <div className='add-practitioners-container'>
+        {/* <Tab data={data} /> */}
         <h1>Practitioner Information</h1>
         <form onSubmit={handleAddPractitioner}>
           <br />
