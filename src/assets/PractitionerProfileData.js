82dd5e9ae99134404684b0ac5458fdba4a0724e8
david-commit <davidondiege@gmail.com>
2023-03-27 23:41:00 +0300

    Created practitioner dummy data for render

---
+++ src/assets/PractitionerProfileData.js
@@ -0,0 +1,184 @@
+[
+  {
+    id: 1,
+    first_name: 'David',
+    last_name: 'Ondiege',
+    image: 'https://i.postimg.cc/pLQ1G3Xm/black-doc-cropped.png',
+    practitioner: {
+      id: 1,
+      first_name: 'David',
+      last_name: 'Ondiege',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 2,
+    first_name: 'Reynalda',
+    last_name: 'Hettinger',
+    image: 'https://i.postimg.cc/j22dSzCb/black-female-nurse-smiling-face.png',
+    practitioner: {
+      id: 2,
+      first_name: 'Reynalda',
+      last_name: 'Hettinger',
+      department: {
+        id: 1,
+        name: 'Mental Health',
+      },
+    },
+  },
+  {
+    id: 3,
+    first_name: 'Erlinda',
+    last_name: 'McGlynn',
+    image: 'https://i.postimg.cc/LX6Zjghy/dark-female-doctor-face.png',
+    practitioner: {
+      id: 3,
+      first_name: 'Erlinda',
+      last_name: 'McGlynn',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 4,
+    first_name: 'Kori',
+    last_name: 'Hermiston',
+    image: 'https://i.postimg.cc/RVCbqgMf/black-male-doc-with-tab-face.png',
+    practitioner: {
+      id: 4,
+      first_name: 'Kori',
+      last_name: 'Hermiston',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 5,
+    first_name: 'Dewey',
+    last_name: 'Daugherty',
+    image: 'https://i.postimg.cc/kMNg7g4G/black-canada-nurse-cropped.png',
+    practitioner: {
+      id: 5,
+      first_name: 'Dewey',
+      last_name: 'Daugherty',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 6,
+    first_name: 'Wilber',
+    last_name: 'Jakubowski',
+    image: 'https://i.postimg.cc/mrQ7RGFs/black-nurse-pointing-cropped.png',
+    practitioner: {
+      id: 6,
+      first_name: 'Wilber',
+      last_name: 'Jakubowski',
+      department: {
+        id: 3,
+        name: 'Cancer',
+      },
+    },
+  },
+  {
+    id: 7,
+    first_name: 'Bert',
+    last_name: 'Harvey',
+    image: 'https://i.postimg.cc/MpwQDFF3/Circilar-balck-male-focused.png',
+    practitioner: {
+      id: 7,
+      first_name: 'Bert',
+      last_name: 'Harvey',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 8,
+    first_name: 'Andera',
+    last_name: 'Moen',
+    image: 'https://i.postimg.cc/nrVWB2tX/circular-black-doc.png',
+    practitioner: {
+      id: 8,
+      first_name: 'Andera',
+      last_name: 'Moen',
+      department: {
+        id: 3,
+        name: 'Cancer',
+      },
+    },
+  },
+  {
+    id: 9,
+    first_name: 'Noelia',
+    last_name: 'Hagenes',
+    image: 'https://i.postimg.cc/V5Qb2GL6/dark-male-doctor.png',
+    practitioner: {
+      id: 9,
+      first_name: 'Noelia',
+      last_name: 'Hagenes',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 10,
+    first_name: 'Xavier',
+    last_name: 'Torp',
+    image: 'https://i.postimg.cc/Xq3r8Z2G/black-male-doctor-smiling.png',
+    practitioner: {
+      id: 10,
+      first_name: 'Xavier',
+      last_name: 'Torp',
+      department: {
+        id: 3,
+        name: 'Cancer',
+      },
+    },
+  },
+  {
+    id: 11,
+    first_name: 'Jorge',
+    last_name: 'Oberbrunner',
+    image:
+      'https://i.postimg.cc/FsrMt98j/black-female-doc-with-file-cropped.png',
+    practitioner: {
+      id: 11,
+      first_name: 'Jorge',
+      last_name: 'Oberbrunner',
+      department: {
+        id: 2,
+        name: 'Diabetes',
+      },
+    },
+  },
+  {
+    id: 12,
+    first_name: 'Francis',
+    last_name: 'Haley',
+    image:
+      'https://i.postimg.cc/nrWR23zz/front-view-nurse-wearing-stethoscope.jpg',
+    practitioner: {
+      id: 12,
+      first_name: 'Francis',
+      last_name: 'Haley',
+      department: {
+        id: 3,
+        name: 'Cancer',
+      },
+    },
+  },
+];
